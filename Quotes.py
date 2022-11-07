ca8710422bbac1f6e397369f703e4aeed5fab5f8
Vamsi Krishna Reddy Munnangi <mvamsimvkr@gmail.com>
2022-11-07 13:02:45 -0500

    Connect to Database

--- Quotes.py
+++ Quotes.py
@@ -1,8 +1,15 @@
 import numpy as np
 import pandas as pd
 import requests
+import mysql.connector
+mydb = mysql.connector.connect(
+    host = "localhost",
+    username = "root",
+    password = ""
+)
+print(mydb)
 for i in range(1,31):
     url="https://dummyjson.com/quotes/"+str(i)
     response = requests.get(url)
     df=pd.DataFrame(response.json(),index=[0])[['id','quote','author']]
-    print(df.values.tolist())
+    # print(df.values.tolist())
