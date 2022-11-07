8c15dc91a4cfdfac210729b77efb0c4041207e70
Vamsi Krishna Reddy Munnangi <mvamsimvkr@gmail.com>
2022-11-07 15:35:37 -0500

    Drop quotes before dropping users

--- Users.py
+++ Users.py
@@ -8,8 +8,8 @@ mydb = mysql.connector.connect(
     password = "",
     database = "Project"
 )
-print(mydb)
 mycursor = mydb.cursor()
+mycursor.execute("drop table if exists quotes")
 mycursor.execute("drop table if exists Users")
 mycursor.execute("create table Users(id int AUTO_INCREMENT PRIMARY KEY,firstName varchar(20),lastName varchar(20),email varchar(20),phone varchar(15),userName varchar(10),password varchar(15))")
 for i in range(1,31):
